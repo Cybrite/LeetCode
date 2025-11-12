48eaa929d3d11285b956793bab290167ff127468
Harsh <140698710+Cybrite@users.noreply.github.com>
2025-11-13 00:57:00 +0530

    Time: 8 ms (74.87%), Space: 9.4 MB (80.37%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Odd-length palindrome
+            expandFromCenter(s, i, i, start, maxLen);
+            // Even-length palindrome
+            expandFromCenter(s, i, i + 1, start, maxLen);
+        }
+
+        return s.substr(start, maxLen);
+    }
+
+private:
+    void expandFromCenter(const string& s, int left, int right, int& start, int& maxLen) {
+        while (left >= 0 && right < s.length() && s[left] == s[right]) {
+            int len = right - left + 1;
+            if (len > maxLen) {
+                maxLen = len;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+};
