5d8e02737040d6d6be92933e86a88de6fe8f7c03
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-03-25 22:36:38 +0530

    Time: 0 ms (100%), Space: 8.9 MB (35.43%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int r = n;
-        if(r == 1) return 1;
-        for(int i = 1; i<=n; i++){
-            if(r >= i){
-                r -= i;
+        long left = 0, right = n;
+
+        while(left <= right){
+            long mid = left+(right-left)/2;
+            long coins = mid*(mid+1)/2;
+
+            if(coins == n){
+                return mid;
+            }
+
+            if(coins < n){
+                left = mid + 1;
             }else{
-                return i-1;
+                right = mid - 1;
             }
         }
-        return r;
+
+        return right;
     }
 };
