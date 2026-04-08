9a91ce0ac8dce6152d6df0a86ca23a2dbfc50440
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-04-08 15:26:22 +0530

    Time: 11 ms (80.92%), Space: 57.1 MB (82.39%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,35 +1,74 @@
 /**
  * Definition for singly-linked list.
  * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * int val;
+ * ListNode *next;
+ * ListNode() : val(0), next(nullptr) {}
+ * ListNode(int x) : val(x), next(nullptr) {}
+ * ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if(head == NULL) return head;
+        // Base case: If the list is empty or has only 1 node, it's already sorted.
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        // 1. Find the middle of the list
+        ListNode* mid = getMid(head);
         
-        ListNode* temp = head;
-        vector<int> arr;
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        // 2. Split the list into two halves
+        ListNode* left = head;
+        ListNode* right = mid->next;
+        mid->next = nullptr; // Sever the link to break it into two independent lists
+
+        // 3. Recursively sort both halves
+        left = sortList(left);
+        right = sortList(right);
+
+        // 4. Merge the sorted halves
+        return merge(left, right);
+    }
+
+private:
+    // Helper function to find the middle using Fast & Slow pointers
+    ListNode* getMid(ListNode* head) {
+        ListNode* slow = head;
+        // Start fast one step ahead so we get the *first* middle node in an even-length list
+        ListNode* fast = head->next; 
+        
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    // Helper function to merge two sorted linked lists in O(1) space
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0); // Dummy node to easily build the new list
+        ListNode* tail = &dummy;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                tail->next = list1;
+                list1 = list1->next;
+            } else {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next; // Move the tail forward
         }
 
-        sort(arr.begin(), arr.end());
-
-        ListNode* head1 = new ListNode(arr[0]);
-        ListNode* mover = head1;
-        for(int i=1; i<arr.size(); i++){
-            ListNode* temp = new ListNode(arr[i]);
-            mover->next = temp;
-            mover = temp;
+        // If one of the lists is not empty, append the rest of it
+        if (list1 != nullptr) {
+            tail->next = list1;
+        } else {
+            tail->next = list2;
         }
 
-        return head1;
+        return dummy.next;
     }
 };
