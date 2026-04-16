3398733c8d8efc28012b36e704d877f94d3755ef
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-04-16 08:42:07 +0530

    Time: 15 ms (15.8%), Space: 44.8 MB (47.35%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
+
+        int i=0, j=0, cnt=0;
+
+        while(i<s.size() && j<g.size()){
+            if(s[i] >= g[j]){
+                cnt++;
+                j++;
+            }
+            i++;
+        }
+
+        return cnt;
+    }
+};
