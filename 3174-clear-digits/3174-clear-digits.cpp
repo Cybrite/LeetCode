01f3789371253eb80e4491ac37a43627a0c056ec
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-05-16 11:36:59 +0530

    Time: 0 ms (100%), Space: 8.5 MB (51.9%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res = "";
+
+        for(int i=0; i<s.length(); i++){
+            if(isdigit(s[i])){
+                if(!res.empty()){
+                    res.pop_back();
+                }
+            }else{
+                res.push_back(s[i]);
+            }
+        }
+
+        return res;
+    }
+};
