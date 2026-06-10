537f37b63519d55b1fee281c31cac175acc59327
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-06-10 15:49:05 +0530

    Time: 0 ms (100%), Space: 10.6 MB (67.85%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,42 +1,50 @@
 class Solution {
 public:
-    vector<int> solve(string expr){
+    unordered_map<string, vector<int>> memo;
+
+    vector<int> solve(string expr) {
+
+        if(memo.count(expr))
+            return memo[expr];
+
         vector<int> ans;
 
-        for(int i=0; i<expr.size(); i++){
+        for(int i = 0; i < expr.size(); i++) {
+
             char c = expr[i];
 
-            if(c == '-' || c == '+' || c == '*'){
-                string left = expr.substr(0,i);
-                string right = expr.substr(i+1);
+            if(c == '+' || c == '-' || c == '*') {
 
-                vector<int> L = solve(left);         
+                string left = expr.substr(0, i);
+                string right = expr.substr(i + 1);
+
+                vector<int> L = solve(left);
                 vector<int> R = solve(right);
 
-                for(int a : L){
-                    for(int b : R){
-                        if(c == '+'){
-                            ans.push_back(a+b);
-                        }else if(c == '-'){
-                            ans.push_back(a-b);
-                        }else{
-                            ans.push_back(a*b);
-                        }
+                for(int a : L) {
+                    for(int b : R) {
+
+                        if(c == '+')
+                            ans.push_back(a + b);
+
+                        else if(c == '-')
+                            ans.push_back(a - b);
+
+                        else
+                            ans.push_back(a * b);
                     }
-                }         
+                }
             }
         }
 
-        if(ans.empty()){
+        if(ans.empty()) {
             ans.push_back(stoi(expr));
         }
 
-        return ans;
+        return memo[expr] = ans;
     }
 
     vector<int> diffWaysToCompute(string expression) {
-        // REMEMBER
-        // split then use recursion to find all possiblilities
         return solve(expression);
     }
 };
