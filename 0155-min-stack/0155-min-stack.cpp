7f2fc4dcf257d46c106e3a512f047b3028e03246
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-07-28 21:35:48 +0530

    Time: 59 ms (50.1%), Space: 151.4 MB (24.54%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -6,11 +6,11 @@ public:
         
     }
     
-    void push(int val) {
-        st1.push(val);
+    void push(int value) {
+        st1.push(value);
 
-        if(st2.empty() || val <= st2.top()){
-            st2.push(val);
+        if(st2.empty() || value <= st2.top()){
+            st2.push(value);
         }
     }
     
@@ -34,7 +34,7 @@ public:
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(val);
+ * obj->push(value);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
