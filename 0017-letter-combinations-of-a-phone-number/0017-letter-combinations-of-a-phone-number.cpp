e9c1b056687c32764cc59263bfeeae66018b48e8
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-08-17 08:53:40 +0530

    Time: 0 ms (100%), Space: 9.5 MB (20.1%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    vector<string> result;
-    
-    void backtrack(string digits, int index, string current, 
-                   unordered_map<char, string>& phone) {
-        
-        if(index == digits.size()) {
-            result.push_back(current);
+    vector<string> res;
+
+    void backtrack(string digits, int idx, string curr, unordered_map<char, string>& nums){
+        if(idx == digits.size()){
+            res.push_back(curr);
             return;
         }
 
-        string letters = phone[digits[index]];
+        string letters = nums[digits[idx]];
 
-        for(char c : letters) {
-            backtrack(digits, index + 1, current + c, phone);
+        for(auto& c : letters){
+            backtrack(digits, idx+1, curr+c, nums);
         }
     }
 
     vector<string> letterCombinations(string digits) {
         if(digits.empty()) return {};
 
-        unordered_map<char, string> phone = {
-            {'2',"abc"}, {'3',"def"}, {'4',"ghi"},
-            {'5',"jkl"}, {'6',"mno"}, {'7',"pqrs"},
-            {'8',"tuv"}, {'9',"wxyz"}
+        unordered_map<char, string> nums = {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+            {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+            {'8', "tuv"}, {'9', "wxyz"}
         };
 
-        backtrack(digits, 0, "", phone);
-        return result;
+        backtrack(digits, 0, "", nums);
+        return res;
     }
 };
