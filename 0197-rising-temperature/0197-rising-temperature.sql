dcbf874781a16a97f2a5eb1569d73cfd502e5fbb
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-08-19 10:54:11 +0530

    Time: 541 ms (39.63%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,4 +2,4 @@
 SELECT today.id FROM Weather today
 INNER JOIN Weather yesterday
 ON DATEDIFF(today.recordDate, yesterday.recordDate) = 1
-WHERE today.temperature > yesterday.temperature;
+AND today.temperature > yesterday.temperature;
