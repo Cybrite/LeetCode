57d802dc16ee62d8ca6503091eb90b951bdc4686
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-09-02 08:53:49 +0530

    Time: 0 ms (100%), Space: 10 MB (90.3%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int p1 = 0, p2 = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            int curr = max(nums[i] + p2, p1);
+            p2 = p1;
+            p1 = curr;
+        }
+
+        return p1;
+    }
+};
