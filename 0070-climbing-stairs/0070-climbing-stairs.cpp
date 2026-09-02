b162f174fd8d32c568463b7b5173997c58c007cd
Harsh <140698710+Cybrite@users.noreply.github.com>
2026-09-02 08:29:32 +0530

    Time: 0 ms (100%), Space: 7.8 MB (88.51%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int x=1, y=2;
-        if(n<=2) return n;
+        int x = 1, y = 2;
+        if(n <= 2) return n;
 
         for(int i=3; i<=n; i++){
-            int curr = x + y;
+            int step = x + y;
             x = y;
-            y = curr;
+            y = step;
         }
-        
+
         return y;
     }
 };
